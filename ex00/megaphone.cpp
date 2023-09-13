5a876bf4a26766376395ab8277ac06bc3ada82ff
Vlad-PLK <leonpolo365@gmail.com>
2023-09-13 09:45:36 +0200

    oop megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,41 +6,47 @@
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:18:17 by vpolojie          #+#    #+#             */
-/*   Updated: 2023/09/12 14:46:27 by vpolojie         ###   ########.fr       */
+/*   Updated: 2023/09/13 09:44:26 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
+class Megaphone
+{
+public:
+    char    *str;
+
+    void    ft_toupper(void)
+    {
+        int i;
+
+        i = 0;
+        while (str[i] != '\0')
+        {
+            str[i] = toupper(str[i]);
+            std::cout << str[i];
+            i++;
+        }
+    }
+};
+
 int main(int argc, char **argv)
 {
-    int     i;
-    int     j;
-    char    c;
+    Megaphone m_phone;
+
+    int i;
 
     i = 1;
     if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
-        while (argv[i] != 0)
+        while (argv[i])
         {
-            j = 0;
-            while (argv[i][j] != '\0')
-            {
-                if (97 <= argv[i][j] && argv[i][j] <= 122)
-                {
-                    c = argv[i][j] - 32;
-                    std::cout << c ;
-                }
-                else
-                {
-                    c = argv[i][j];
-                    std::cout << c ;
-                }
-                j++;
-            }
+            m_phone.str = argv[i];
+            m_phone.ft_toupper();
             i++;
         }
         std::cout << "\n";
